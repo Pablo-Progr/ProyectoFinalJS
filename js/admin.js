a60dddb8a309b68af27d358ea183c57a8ca4c21c
Pablogd <dominguezpg.99@gmail.com>
2024-11-12 16:41:13 -0300

    cambiosproductos.js, admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,14 +1,16 @@
 const handleClick = async (e) => {
   e.preventDefault();
 
-  try {
-    let categoria = document.getElementById("categoriaProducto").value;
-    let marca = document.getElementById("marcaProducto").value;
-    let producto = document.getElementById("nombreProducto").value;
-    let precio = document.getElementById("precioProducto").value;
-    let cantidad = document.getElementById("tamañoProducto").value;
+  let categoria = document.getElementById("categoriaProducto").value;
+  let marca = document.getElementById("marcaProducto").value;
+  let producto = document.getElementById("nombreProducto").value;
+  let precio = document.getElementById("precioProducto").value;
+  let cantidad = document.getElementById("tamañoProducto").value;
+  let imagen = document.getElementById("imagenProducto").value;
 
-    e.target.reset();
+  e.target.reset();
+
+  try {
 
     let response = await axios.post("http://localhost:3001/productos", {
       producto,
@@ -16,6 +18,7 @@ const handleClick = async (e) => {
       categoria,
       precio,
       cantidad,
+      imagen
     });
 
     console.log(response);
@@ -34,6 +37,8 @@ const handleClick = async (e) => {
   }
 };
 
+
+
 const eliminarProducto = async(id) => {
     console.log(id)
     try {
@@ -51,6 +56,10 @@ const eliminarProducto = async(id) => {
     
 }
 
+const mostrarProducto = async(imagen) => {
+  let response = await axios.get("http://localhost:3001/productos");
+}
+
 const getTablaProductos = async () => {
   try {
 
@@ -65,7 +74,7 @@ const getTablaProductos = async () => {
              <td>${producto.cantidad}</td>
              <td>$${producto.precio}</td>
              <td class="iconos">
-              <button class="icono icono-mirar">
+              <button class="icono icono-mirar" onClick="mostrarProducto('${producto.imagen}')">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
                  <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/>
                  <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/>
@@ -94,5 +103,4 @@ const getTablaProductos = async () => {
 getTablaProductos()
 
 document
-  .getElementById("guardarProducto")
-  .addEventListener("submit", handleClick);
+  .getElementById("guardarProducto").addEventListener("submit", handleClick);
