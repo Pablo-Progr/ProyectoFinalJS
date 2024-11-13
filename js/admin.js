9e7a0ae9bbe4ef53436eec64806ec1e88ecf5eb2
Pablogd <dominguezpg.99@gmail.com>
2024-11-13 12:18:40 -0300

    crear, editar y eliminar producto listo

--- js/admin.js
+++ js/admin.js
@@ -1,24 +1,30 @@
-const handleClick = async (e) => {
-  e.preventDefault();
+let ID = ""
+
+const handleClick = () => {
+  document.getElementById("primerBtnAgregar").style.display = "none";
+  document.getElementById("segundoBtnAgregar").style.display = "block";
+  document.getElementById("guardarProducto").style.display = "block";
+  
+}
+
+const agregarProducto = async () => {
 
   let categoria = document.getElementById("categoriaProducto").value;
   let marca = document.getElementById("marcaProducto").value;
   let producto = document.getElementById("nombreProducto").value;
   let precio = document.getElementById("precioProducto").value;
-  let cantidad = document.getElementById("tamañoProducto").value;
+  let tamanio = document.getElementById("tamañoProducto").value;
   let imagen = document.getElementById("imagenProducto").value;
 
-  e.target.reset();
 
   try {
-
     let response = await axios.post("http://localhost:3001/productos", {
       producto,
       marca,
       categoria,
       precio,
-      cantidad,
-      imagen
+      tamanio,
+      imagen,
     });
 
     console.log(response);
@@ -37,51 +43,135 @@ const handleClick = async (e) => {
   }
 };
 
+const eliminarProducto = async (id) => {
 
+  ID = id
+  console.log(id);
+  try {
+    let response = await axios.delete("http://localhost:3001/productos/" + ID);
 
-const eliminarProducto = async(id) => {
-    console.log(id)
-    try {
-        let response = await axios.delete("http://localhost:3001/productos/"+id)
-
-        if(response){
-            alert("producto eliminado")
-        }else{
-            alert("No se pudo eliminar el producto")
-        }
-    } catch (error) {
-        
+    if (response) {
+      alert("producto eliminado");
+    } else {
+      alert("No se pudo eliminar el producto");
     }
+  } catch (error) {
+    console.log(error)
+  }
+};
 
+const editarProducto = (id,marca,producto,tamanio,precio,imagen) =>{
+
+  ID = id
+  console.log(ID)
+  document.getElementById("guardarProducto").style.display = "block";
+  document.getElementById("botonEditar").style.display = "block";
+  document.getElementById("primerBtnAgregar").style.display = "none";
+  document.getElementById("segundoBtnAgregar").style.display = "none";
+
+ 
+  document.getElementById("marcaProducto").value = marca ;
+  document.getElementById("nombreProducto").value = producto;
+  document.getElementById("tamañoProducto").value = tamanio;
+  document.getElementById("precioProducto").value = precio;
+  document.getElementById("imagenProducto").value = imagen;
+
+}
+
+const ActualizarProducto = async () => {
+  
+  try {
     
+    let marca = document.getElementById("marcaProducto").value;
+    let producto = document.getElementById("nombreProducto").value;
+    let categoria = document.getElementById("categoriaProducto").value;
+    let tamanio = document.getElementById("tamañoProducto").value;
+    let precio = document.getElementById("precioProducto").value;
+    let imagen = document.getElementById("imagenProducto").value;
+
+    console.log(ID)
+    let response = await axios.put("http://localhost:3001/productos/" + ID,{
+      marca,
+      producto,
+      categoria,
+      tamanio,
+      precio,
+      imagen
+    })
+
+    if (response) {
+      alert("producto actualizado");
+    } else {
+      alert("No se pudo actualizar el producto");
+    }
+  
+  } catch (error) {
+    console.log(error)
+  }
+
+  
 }
 
-const mostrarProducto = async(imagen) => {
-  let response = await axios.get("http://localhost:3001/productos");
-}
+const mostrarProducto = async (id) => {
+  try {
+    let response = await axios.get("http://localhost:3001/productos");
+    console.log(id)
+
+    document.getElementById("modalImagenProducto").innerHTML += `
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">
+                  ${data.producto}
+                </h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <label class="form-label">Nombre Usuario</label>
+                <input type="text" class="form-control">
+                <label class="form-label">Contraseña</label>
+                <input type="password" class="form-control">
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary " style="border: 1px solid black;" data-bs-dismiss="modal" >
+                  Close
+                </button>
+                <button type="button" class="btn btn-success" style="border: 1px solid black;">
+                  Iniciar sesion
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+  `;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const getTablaProductos = async () => {
   try {
-
     let response = await axios.get("http://localhost:3001/productos");
-    response.data.map((producto) =>
+    response.data.map(
+      (producto) =>
         (document.getElementById("tablaProductos").innerHTML += `
         <tr>
             <th scope="row">${producto.id}</th>
              <td>${producto.marca}</td>
              <td>${producto.producto}</td>
              <td>${producto.categoria}</td>
-             <td>${producto.cantidad}</td>
+             <td>${producto.tamanio}</td>
              <td>$${producto.precio}</td>
              <td class="iconos">
-              <button class="icono icono-mirar" onClick="mostrarProducto('${producto.imagen}')">
+              <button class="icono icono-mirar" onClick="mostrarProducto('${producto}')">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
                  <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/>
                  <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/>
                 </svg>
              </button>
              <button class="icono icono-editar"> 
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" onClick="editarProducto('${producto.id}','${producto.marca}','${producto.producto}','${producto.tamanio}','${producto.precio}','${producto.imagen}')" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                      <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                      <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
                 </svg>
@@ -96,11 +186,14 @@ const getTablaProductos = async () => {
     `)
     );
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 };
 
-getTablaProductos()
+getTablaProductos();
 
-document
-  .getElementById("guardarProducto").addEventListener("submit", handleClick);
+document.getElementById("segundoBtnAgregar").addEventListener("click", agregarProducto);
+
+document.getElementById("primerBtnAgregar").addEventListener("click", handleClick);
+
+document.getElementById("botonEditar").addEventListener("click", ActualizarProducto)
--- js/productos.js
+++ js/productos.js
@@ -23,3 +23,4 @@ const mostrarProductos = async () => {
 
 
 mostrarProductos();
+  
