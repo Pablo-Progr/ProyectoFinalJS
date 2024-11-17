7d76e7d486089b61036193753c2f4fa34efff0e6
Pablo <dominguezpg.99@gmail.com>
2024-11-17 01:56:42 -0300

    footer

--- js/productos.js
+++ js/productos.js
@@ -7,11 +7,12 @@ const mostrarProductos = async () => {
             <div class="card card-bebida" style="width: 18rem">
                 <img src="${producto.imagen}" class="card-img" alt="Imagen Campari" style="height: 340px;"/>
                 <div class="card-body">
-                    <h5 class="card-title">${producto.producto}6</h5>
+                    <h5 class="card-title">${producto.categoria} -  ${producto.producto}6</h5>
                     <h5 class="card-text">$${producto.precio}</h5>
-                    <div class="agregar-carrito row">
-                    <input class="col-4" type="number" placeholder="0" style="height: 35px;">
-                    <button class="btn btn-success col-6">Agregar al carrito</button>
+                    <div class="agregar-carrito">
+                        <input class="col-4" type="number" placeholder="0" style="height: 35px;">
+                        <button class="btn btn-success ">Agregar al carrito</button>
+                    </div>
                 </div>
             </div>
       `)
